da3ea94e4c33c9aaa073053a2c9cdded30c6d7ac
RRALALS7 <davibarboza520@gmail.com>
2026-04-24 19:29:08 -0300

    feat(music-station): add radio presets

---
+++ complexos/music-station-bot/src/config/radios.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    id: 'lofi-demo',
+    name: 'Lofi Demo Stream',
+    url: 'https://stream.zeno.fm/0r0xa792kwzuv'
+  },
+  {
+    id: 'chill-demo',
+    name: 'Chill Demo Stream',
+    url: 'https://stream.zeno.fm/f3wvbbqmdg8uv'
+  }
+];
